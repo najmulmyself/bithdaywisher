46089d734665d97f5e5773bff1758697df2b17c8
najmulmyself <najmul.myself@gmail.com>
2022-06-12 01:49:53 +0600

    simple smtp done

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ import datetime as dt
 import random
 import pandas
 
+
+MY_EMAIL = 'najmul.test@gmail.com'
+MY_PASSWORD = '4141234235'
+
 today = dt.now()
 today_tuple = (today.month, today.day)
 
@@ -33,3 +37,19 @@ data = pandas.read_csv("birthdays.csv")
 
 
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/leter_{random.randint(1,3)}.text"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!\n\n {contents}")
